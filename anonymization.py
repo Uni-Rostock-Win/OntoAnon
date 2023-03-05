44cb7d990ff55c9199d365c00254094ef58fb5e2
Junech1 <rs-airbrush@gmx.de>
2023-03-05 18:11:26 +0100

    Update anonymization.py
    Changes to recognize standard vocabulary

--- anonymization.py
+++ anonymization.py
@@ -1,9 +1,14 @@
 from tkinter import messagebox
+
+import rdflib
 from rdflib import Graph, Literal, URIRef
+from rdflib.namespace import Namespace
+
 
 def anonymize(filename, fileformat, anony_path, dict_path):
     # Create a Graph
     g = Graph()
+    all_ns = [ns for ns in rdflib.namespace._NAMESPACE_PREFIXES_RDFLIB.values()] + [str(ns) for ns in rdflib.namespace._NAMESPACE_PREFIXES_CORE.values()]
 
     try:
         # Parse in an ontologie file
@@ -21,32 +26,29 @@ def anonymize(filename, fileformat, anony_path, dict_path):
 
     # translate the namespaces
     namespace_translator = []
-    namespace_to_generic_namespace(namespaces, namespace_translator)
+    namespace_to_generic_namespace(namespaces, namespace_translator, all_ns)
     translator.extend(namespace_translator)
-    print(namespace_translator)
 
     # translate the subjects
     subject_translator = []
-    subject_to_generic_subject(subjects, subject_translator, namespace_translator)
+    subject_to_generic_subject(subjects, subject_translator, namespace_translator, all_ns)
     translator.extend(subject_translator)
 
     # translate the predicates
     predicat_translator = []
-    predicat_to_generic_predicat(predicates, predicat_translator, namespace_translator)
+    predicat_to_generic_predicat(predicates, predicat_translator, namespace_translator, all_ns)
     translator.extend(predicat_translator)
 
     # translate the objects
     object_translator = []
-    object_to_generic_object(objects, object_translator, namespace_translator)
+    object_to_generic_object(objects, object_translator, namespace_translator,all_ns)
     translator.extend(object_translator)
 
     # Replaces the subjects, predicates and objects in the graph with the anonymized elements
     change_graph(g,subject_translator,predicat_translator,object_translator)
 
     # Saving the anonymized graph to the selected file
-    new_graph = str(g.serialize(format=fileformat).decode('utf-8'))
-    new_graph = new_graph[2:]
-    new_graph = new_graph[:-3]
+    new_graph = g.serialize(format=fileformat)
 
     with open(anony_path, 'w') as fp:
         fp.write(new_graph)
@@ -97,30 +99,44 @@ def identify_elements(g):
     return namespaces, objects, predicates, subjects
 
 # translates the namespaces to generic namespaces
-def namespace_to_generic_namespace(namespaces, translator):
+def namespace_to_generic_namespace(namespaces, translator, all_ns):
     name_counter = 0
+    standard_ns = False
+
     for name_element in namespaces:
+        # check if namespace standard namespace
+        for ns in all_ns:
+            if (URIRef(name_element[1]) in Namespace(ns)) :
+                standard_ns = True
+                translator.append([name_element, name_element])
+
         # check if namespace has an URL and translate it with an URIRef, otherwise it becomes a Literal
-        if 'http' in name_element[1]:
+        if ('http' in name_element[1]) and (standard_ns == False):
             if '#' in name_element[1]:
                 translator.append([name_element, ("Namespace" + str(name_counter),
-                                                  URIRef(
-                                                      "http://anonym-url.anon/Namespace" + str(name_counter) + "#"))])
+                                                  URIRef("http://anonym-url.anon/Namespace" + str(name_counter) + "#"))])
             else:
                 translator.append([name_element, ("Namespace" + str(name_counter),
                                                   URIRef("http://anonym-url.anon/Namespace" + str(name_counter) + "/"))])
-        else:
+        elif standard_ns == False:
             translator.append([name_element, Literal("Namespace" + str(name_counter))])
         name_counter = name_counter + 1
 
+        standard_ns = False
+
 # translates the subjects to generic subjects
-def subject_to_generic_subject(subjects, subject_translator, namespace_translator):
+def subject_to_generic_subject(subjects, subject_translator, namespace_translator, all_ns):
     subj_counter = 0
+    standard_ns = False
     for subj_element in subjects:
+        #check if namespace standard namespace
+        for ns in all_ns:
+            if (URIRef(subj_element) in Namespace(ns)) :
+                standard_ns = True
+                subject_translator.append([subj_element, subj_element])
 
         # check if subject contains an URIRef, if not translate it as normal Literal
-        if 'http' in subj_element:
-
+        if ('http' in subj_element) and (standard_ns == False):
             # check if URIRef is a known namespace
             is_namespace = False
             namespace_count = 0
@@ -133,21 +149,31 @@ def subject_to_generic_subject(subjects, subject_translator, namespace_translato
             # if URIRef is a known namespace translate it accordingly
             # if not translate it as normal URIRef
             if is_namespace == True:
-                new_string = str(namespace_translator[namespace_count][1][1][:-1] + '/Subject' + str(subj_counter))
+                new_string = str(namespace_translator[namespace_count][1][1] + 'Subject' + str(subj_counter))
                 subject_translator.append([subj_element, URIRef(new_string)])
             else:
-                subject_translator.append([subj_element, URIRef("http://anonym-url.anon/Subject" + str(subj_counter))])
-        else:
+                if '#' in subj_element:
+                    subject_translator.append([subj_element, URIRef("http://anonym-subj-url.anon#Subject" + str(subj_counter))])
+                else:
+                    subject_translator.append([subj_element, URIRef("http://anonym-subj-url.anon/Subject" + str(subj_counter))])
+        elif standard_ns == False:
             subject_translator.append([subj_element, Literal("Subject" + str(subj_counter))])
         subj_counter = subj_counter + 1
 
-# translates the predicates to generic predicates
-def predicat_to_generic_predicat(predicates, predicat_translator, namespace_translator):
-    predicat_counter = 0
-    for pred_element in predicates:
-        # check if predicate contains an URIRef, if not translate it as normal Literal
-        if 'http' in pred_element:
+        standard_ns = False
 
+# translates the predicates to generic predicates
+def predicat_to_generic_predicat(predicates, predicat_translator, namespace_translator, all_ns):
+    predicat_counter = 0
+    standard_ns = False
+    for pred_element in predicates:
+        # check if namespace standard namespace
+        for ns in all_ns:
+            if (URIRef(pred_element) in Namespace(ns)) :
+                standard_ns = True
+                predicat_translator.append([pred_element, pred_element])
+
+        if ('http' in pred_element) and (standard_ns == False):
             # check if URIRef is a known namespace
             is_namespace = False
             namespace_count = 0
@@ -163,18 +189,27 @@ def predicat_to_generic_predicat(predicates, predicat_translator, namespace_tran
                 new_string = str(namespace_translator[namespace_count][1][1] + 'Predicate' + str(predicat_counter))
                 predicat_translator.append([pred_element, URIRef(new_string)])
             else:
-                predicat_translator.append([pred_element, URIRef("http://anonym-url.anon/Predicate" + str(predicat_counter))])
-        else:
+                if '#' in pred_element:
+                    predicat_translator.append([pred_element, URIRef("http://anonym-pred-url.anon#Predicate" + str(predicat_counter))])
+                else:
+                    predicat_translator.append([pred_element, URIRef("http://anonym-pred-url.anon/Predicate" + str(predicat_counter))])
+        elif standard_ns == False:
             predicat_translator.append([pred_element, Literal("Predicate" + str(predicat_counter))])
         predicat_counter = predicat_counter + 1
 
-# translates the objects to generic objects
-def object_to_generic_object(objects, object_translator, namespace_translator):
-    object_counter = 0
-    for obj_element in objects:
+        standard_ns = False
 
-        # check if pbjecgs contains an URIRef, if not translate it as normal Literal
-        if 'http' in obj_element:
+# translates the objects to generic objects
+def object_to_generic_object(objects, object_translator, namespace_translator,all_ns):
+    object_counter = 0
+    standard_ns = False
+    for obj_element in objects:
+        for ns in all_ns:
+            if (URIRef(obj_element) in Namespace(ns)) :
+                standard_ns = True
+                object_translator.append([obj_element, obj_element])
+
+        if ('http' in obj_element) and (standard_ns == False):
 
             # check if URIRef is a known namespace
             is_namespace = False
@@ -182,6 +217,7 @@ def object_to_generic_object(objects, object_translator, namespace_translator):
             for element in namespace_translator:
                 if element[0][1][:-1] in obj_element:
                     is_namespace = True
+                    is_namespace = False
                     break
                 namespace_count = namespace_count +1
 
@@ -191,10 +227,15 @@ def object_to_generic_object(objects, object_translator, namespace_translator):
                 new_string = str(namespace_translator[namespace_count][1][1] + 'Object' + str(object_counter))
                 object_translator.append([obj_element, URIRef(new_string)])
             else:
-                object_translator.append([obj_element, URIRef("http://anonym-url.anon/Object" + str(object_counter))])
-        else:
-            object_translator.append([obj_element, Literal("Object" + str(object_counter))])
+                if '#' in obj_element:
+                    object_translator.append([obj_element, URIRef("http://anonym-obj-url.anon#Object" + str(object_counter))])
+                else:
+                    object_translator.append([obj_element, URIRef("http://anonym-obj-url.anon/Object" + str(object_counter))])
+        elif standard_ns == False:
+                object_translator.append([obj_element, Literal("Object" + str(object_counter))])
         object_counter = object_counter + 1
+
+        standard_ns = False
 
 # Removes the old triple and adds the anonymized triple to the graph
 def change_graph(g,subject_translator,predicat_translator,object_translator):
